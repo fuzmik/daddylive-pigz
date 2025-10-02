8f2c8a6ccc97514d0a65ba5b36a183ab965b2ba2
pigzillaaa <216503871+pigzillaaa@users.noreply.github.com>
2025-10-02 12:24:58 +0800

    Update daddylive-channels-epg-grabber.py

--- epgs/daddylive-channels-epg-grabber.py
+++ epgs/daddylive-channels-epg-grabber.py
@@ -112,6 +112,8 @@ urls = [
     "https://epgshare01.online/epgshare01/epg_ripper_TR1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz",
+    "https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz",
+    "https://epgshare01.online/epgshare01/epg_ripper_CA2.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UY1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz",
